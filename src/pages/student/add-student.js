09659610fbc1d736d00f6b2ea8a265aa7bf8b643
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-18 16:30:23 +0530

    4.1 : add new student

--- src/pages/student/add-student.js
+++ src/pages/student/add-student.js
@@ -1,68 +1,77 @@
 import { useState } from "react";
 import { Input, Row, Col, Button, Card } from "antd";
+import { useAddStudentMutation } from "../../services/students";
 
 const AddStudent = ({ history }) => {
-  const [data, setData] = useState({
-    fullName: "",
-    phone: "",
-    email: "",
-  });
-
-  const handleChange = (e) =>
-    setData({ ...data, [e.target.name]: e.target.value });
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(data);
-
-    // after submit data
-    setData({
-      fullName: "",
-      phone: "",
-      email: "",
+    const [data, setData] = useState({
+        fullName: "",
+        phone: "",
+        email: "",
     });
 
-    history.push("/");
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <Card title="Create a new student">
-        <Row gutter={[0, 20]}>
-          <Col span={24}>
-            <Input
-              size="large"
-              placeholder="Enter Student Full Name"
-              name="fullName"
-              value={data.fullName}
-              onChange={handleChange}
-            />
-          </Col>
-          <Col span={24}>
-            <Input
-              size="large"
-              placeholder="Enter Student Phone Number"
-              name="phone"
-              value={data.phone}
-              onChange={handleChange}
-            />
-          </Col>
-          <Col span={24}>
-            <Input
-              size="large"
-              placeholder="Enter Student E-mail address"
-              name="email"
-              value={data.email}
-              onChange={handleChange}
-            />
-          </Col>
-          <Col span={24}>
-            <Button htmlType="submit" type="primary">
-              Add Student
-            </Button>
-          </Col>
-        </Row>
-      </Card>
-    </form>
-  );
+    const [addStudent, { isLoading }] = useAddStudentMutation();
+
+    const handleChange = (e) => setData({ ...data, [e.target.name]: e.target.value });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        await addStudent(data);
+
+        console.log(data);
+
+        // after submit data
+        setData({
+            fullName: "",
+            phone: "",
+            email: "",
+        });
+
+        history.push("/");
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <Card title="Create a new student">
+                <Row gutter={[0, 20]}>
+                    <Col span={24}>
+                        <Input
+                            size="large"
+                            placeholder="Enter Student Full Name"
+                            name="fullName"
+                            value={data.fullName}
+                            onChange={handleChange}
+                            disabled={isLoading}
+                        />
+                    </Col>
+                    <Col span={24}>
+                        <Input
+                            size="large"
+                            placeholder="Enter Student Phone Number"
+                            name="phone"
+                            value={data.phone}
+                            onChange={handleChange}
+                            disabled={isLoading}
+                        />
+                    </Col>
+                    <Col span={24}>
+                        <Input
+                            size="large"
+                            placeholder="Enter Student E-mail address"
+                            name="email"
+                            value={data.email}
+                            onChange={handleChange}
+                            disabled={isLoading}
+                        />
+                    </Col>
+                    <Col span={24}>
+                        <Button htmlType="submit" type="primary" disabled={isLoading}>
+                            Add Student
+                        </Button>
+                    </Col>
+                </Row>
+            </Card>
+        </form>
+    );
 };
 
 export default AddStudent;
--- src/services/students.js
+++ src/services/students.js
@@ -4,15 +4,30 @@ export const studentsApi = createApi({
     reducerPath: "studentsApi",
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/" }),
 
+    tagTypes: ["Student"],
+
     endpoints: (builder) => ({
+        // get all students list
         getStudents: builder.query({
             query: () => "students",
+            providesTags: ["Student"],
         }),
 
+        // single student
         getStudentById: builder.query({
             query: (id) => `students/${id}`,
+        }),
+
+        // add new student
+        addStudent: builder.mutation({
+            query: (studentData) => ({
+                url: "students",
+                method: "POST",
+                body: studentData,
+            }),
+            invalidatesTags: ["Student"],
         }),
     }),
 });
 
-export const { useGetStudentsQuery, useGetStudentByIdQuery } = studentsApi;
+export const { useGetStudentsQuery, useGetStudentByIdQuery, useAddStudentMutation } = studentsApi;
